2e961453e04eed5bc1d4abd5091bded990dc39b6
har-leen <kau10521@sheridancollege.ca>
2022-02-02 22:27:29 -0500

    branch

--- src/cardtrick/Card.java
+++ src/cardtrick/Card.java
@@ -6,6 +6,15 @@ public class Card {
     private String suit;
     public static final String[] SUITS = {"diamonds", "spades", "clubs", "hearts"};
 
+    public Card(int numbers, String suit) {
+        setNumbers(numbers);
+        setSuits(suit);
+        randomSuit(suit);
+    }
+
+    Card() {
+    }
+
     /**
      * @return the numbers
      */
@@ -17,21 +26,47 @@ public class Card {
      * @param numbers the numbers to set
      */
     public void setNumbers(int numbers) {
+        numbers = 1 + ((int) (Math.random() * 13));
         this.numbers = numbers;
     }
 
     /**
-     * @return the suits
+     * @return the suit
      */
     public String getSuits() {
-        return suit;
+        return this.suit;
+
     }
 
     /**
-     * @param suits the suits to set
+     * @param suit the suit to set
      */
-    public void setSuits(String suits) {
-        this.suit = suits;
+    public void setSuits(String suit) {
+        if (suit.equalsIgnoreCase(SUITS[0]) || suit.equalsIgnoreCase(SUITS[1]) || suit.equalsIgnoreCase(SUITS[2])
+                || suit.equalsIgnoreCase(SUITS[3])) {
+            this.suit = suit;
+        } else {
+            System.out.println("Try again");
+        }
+        
+    }
+
+    public String randomSuit(String suit) {
+        int randomNum = (int) (1 + (Math.random() * 4));
+        if (randomNum == 1) {
+            suit = SUITS[0];
+        } else if (randomNum == 2) {
+            suit = SUITS[1];
+        } else if (randomNum == 3) {
+            suit = SUITS[2];
+        } else if (randomNum == 4) {
+            suit = SUITS[3];
+        } 
+        return this.suit = suit;
+    }
+    
+    public String toString(){
+        return "Number: " + this.numbers + ", Suit: " + this.suit;
     }
 
 }
--- src/cardtrick/CardTrick.java
+++ src/cardtrick/CardTrick.java
@@ -1,23 +1,58 @@
 package cardtrick;
 
+import static cardtrick.Card.SUITS;
+import java.util.Random;
+import java.util.Scanner;
+
 public class CardTrick {
 
     public static void main(String[] args) {
+
+        Card c = new Card();
+        c.getSuits();
+        c.getNumbers();
+        int numbers = 0;
+        String suit = "";
+
+        Card[] magicHand = new Card[7];//Array of Object
+        // Random random = new Random();
+        //int n = random.nextInt(3);
+
+        for (int i = 0; i < magicHand.length; i++) {
+            // c.setNumbers(numbers); //write here random generation fo number or use methods
+            // c.setSuits(SUITS[n]);
+
+            magicHand[i] = new Card();
+            // stores the card object in array
+            magicHand[i].setNumbers(numbers);
+            magicHand[i].randomSuit(suit);
+            // System.out.println(magicHand[i].getNumbers() + ", " + magicHand[i].randomSuit(suit));
+            System.out.println(magicHand[i].toString());
+        }
+
+        Scanner k = new Scanner(System.in);
+        System.out.print("Enter a number bewtween 1 - 13: ");
+        int num = k.nextInt();
+        if (num > -1 && num < 14) {
+            System.out.print("Enter a suit from deck of cards: ");
+            suit = k.next();
+            for (int i = 0; i < 1; i++) {
+                magicHand[i].getSuits();
+            }
+        }
+        for (int i = 0; i < magicHand.length; i++) {
+            if (magicHand[i].getSuits().equals(suit)) {
+                if (magicHand[i].getNumbers() == num) {
+                    System.out.println("Card Found");
+                }
+            }
+        }
         
-      Card [] magicHand = new Card [7]; //Array of Object
-      
-      for(int i=0; i<magicHand.length;i++){
-          Card c = new Card();
-          c.setNumbers(2); //write here random generation fo number or use methods
-          c.setSuits("suit by random suit generation");
-          magicHand[i]=c; // stores the card object in array
-      }
+    }
 // step 2 take input from user, guess the card -- 2 spades -- find in array -- print message card is found -- else not found
 // complete the code and push it to the repository
 // step 3 hard coded card details in RR 
 //like  card no = 2, hearts 
 // find card it array
 // pull it back
-    }
-
 }
